1e3537f6a7de88e7d8b86f2b2a3edcd6f2d825ec
blazinsoul <lukevdloop@hotmail.com>
2013-11-12 17:22:02 +0100

    wegtiefen wat niet nodig was en alle functies alvast aangemaakt in mainclass.

---
+++ ActiveDirectory.cs
@@ -0,0 +1,155 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.DirectoryServices;
+
+namespace FileShare
+{
+    class ActiveDirectory
+    {
+        // om te kijken of iets bestaat (misschien wel handig als je users aan groepen toevoegd hahah)
+        
+        public static bool Exists(string objectPath)
+        {
+            bool found = false;
+            if (DirectoryEntry.Exists("LDAP://" + objectPath))
+            {
+                found = true;
+            }
+            return found;
+        }
+
+        //voor de authenticatie van een gebruiker
+
+        private bool Authenticate(string userName,
+            string password, string domain)
+        {
+            bool authentic = false;
+            try
+            {
+                DirectoryEntry entry = new DirectoryEntry("LDAP://" + domain,
+                    userName, password);
+                object nativeObject = entry.NativeObject;
+                authentic = true;
+            }
+            catch (DirectoryServicesCOMException) { }
+            return authentic;
+        }
+
+        // voor het toevoegen van een user
+
+        public string CreateUserAccount(string ldapPath, string userName,
+            string userPassword)
+        {
+            string oGUID = string.Empty;
+            try
+            {
+                
+                string connectionPrefix = "LDAP://" + ldapPath;
+                DirectoryEntry dirEntry = new DirectoryEntry(connectionPrefix);
+                DirectoryEntry newUser = dirEntry.Children.Add
+                    ("CN=" + userName, "user");
+                newUser.Properties["samAccountName"].Value = userName;
+                newUser.CommitChanges();
+                oGUID = newUser.Guid.ToString();
+
+                newUser.Invoke("SetPassword", new object[] { userPassword });
+                newUser.CommitChanges();
+                dirEntry.Close();
+                newUser.Close();
+            }
+            catch (System.DirectoryServices.DirectoryServicesCOMException E)
+            {
+                Console.WriteLine(E);
+
+            }
+            return oGUID;
+        }
+
+        // voor het toevoegen van een groep
+
+        public void Create(string ouPath, string name)
+        {
+            if (!DirectoryEntry.Exists("LDAP://CN=" + name + "," + ouPath))
+            {
+                try
+                {
+                    DirectoryEntry entry = new DirectoryEntry("LDAP://" + ouPath);
+                    DirectoryEntry group = entry.Children.Add("CN=" + name, "group");
+                    group.Properties["sAmAccountName"].Value = name;
+                    group.CommitChanges();
+                }
+                catch (Exception e)
+                {
+                    Console.WriteLine(e.Message.ToString());
+                }
+            }
+            else { Console.WriteLine(ouPath + " already exists"); }
+        }
+
+        // voor het verwijderen van een groep 
+
+        public void Delete(string ouPath, string groupPath)
+        {
+            if (DirectoryEntry.Exists("LDAP://" + groupPath))
+            {
+                try
+                {
+                    DirectoryEntry entry = new DirectoryEntry("LDAP://" + ouPath);
+                    DirectoryEntry group = new DirectoryEntry("LDAP://" + groupPath);
+                    entry.Children.Remove(group);
+                    group.CommitChanges();
+                }
+                catch (Exception e)
+                {
+                    Console.WriteLine(e.Message.ToString());
+                }
+            }
+            else
+            {
+                Console.WriteLine(ouPath + " doesn't exist");
+            }
+        }
+
+        // voor het toevoegen van een user aan een groep
+
+        public void AddToGroup(string userDn, string groupDn)
+        {
+            try
+            {
+                DirectoryEntry dirEntry = new DirectoryEntry("LDAP://" + groupDn);
+                dirEntry.Properties["member"].Add(userDn);
+                dirEntry.CommitChanges();
+                dirEntry.Close();
+            }
+            catch (System.DirectoryServices.DirectoryServicesCOMException)
+            {
+                
+            }
+        }
+
+        // voor het verwijderen van een user van een groep (dus alleen uit een groep halen)
+
+        public void RemoveUserFromGroup(string userDn, string groupDn)
+        {
+            try
+            {
+                DirectoryEntry dirEntry = new DirectoryEntry("LDAP://" + groupDn);
+                dirEntry.Properties["member"].Remove(userDn);
+                dirEntry.CommitChanges();
+                dirEntry.Close();
+            }
+            catch (System.DirectoryServices.DirectoryServicesCOMException E)
+            {
+                Console.WriteLine(E);
+
+            }
+        }
+        
+
+
+
+    }
+}
